531ea0d62f99392ed72ba8121ed9d317c9bc9a62
refokcer <refokcer@gmail.com>
2024-04-05 09:45:44 +0300

    Added tests with fixtures for  functions to open files

--- test_utils.py
+++ test_utils.py
@@ -1,4 +1,5 @@
 from utils import process_text, count_words, count_sentences
+from tempfile import NamedTemporaryFile
 import pytest
 
 
@@ -22,6 +23,32 @@ def test_count_sentences(content, expected):
     ("Пунктуація, не впливає: на; кількість? слів!", 6),
     ("Одне-два, три-чотири", 2),
 ])
-
 def test_count_words(content, expected):
     assert count_words(content) == expected, f"Помилка у функції count_words для тексту: {content}"
+
+
+# Фікстура для створення тимчасового файлу з заданим вмістом
+@pytest.fixture
+def create_temp_file():
+    def _create_temp_file(content):
+        with NamedTemporaryFile('w+', encoding='utf-8', delete=False) as tmpfile:
+            tmpfile.write(content)
+            tmpfile.flush()
+            return tmpfile.name
+
+    return _create_temp_file
+
+
+# Параметризований тест для функції process_text
+@pytest.mark.parametrize("content,expected_word_count,expected_sentence_count", [
+    ("Один. Два, три; чотири! П'ять?", 5, 3),
+    ("Без слів без речень...", 4, 1),
+    ("Просто речення без знаків пунктуації", 5, 0),
+    ("", 0, 0),  # Порожній файл
+    ("Одне речення. І ще одне... І ще одне!", 8, 3),
+])
+def test_process_text(create_temp_file, content, expected_word_count, expected_sentence_count):
+    filename = create_temp_file(content)
+    word_count, sentence_count = process_text(filename)
+    assert word_count == expected_word_count, f"Очікувалось {expected_word_count} слів, але знайдено {word_count}"
+    assert sentence_count == expected_sentence_count, f"Очікувалось {expected_sentence_count} речень, але знайдено {sentence_count}"
